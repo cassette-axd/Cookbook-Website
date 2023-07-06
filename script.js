23ab06aadf08b0e2f3d2cfcf09148fa490cccfa4
cassette-axd <reyescaseym@gmail.com>
2023-07-06 00:59:35 -0500

    added images to folder and made background change based on time of day

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function displayTime() {
     var mins = dateTime.getMinutes();
     var secs = dateTime.getSeconds();
     let period = 'AM';
+    let clock = document.querySelector('.container');
 
     if (hrs > 12) {
         period = 'PM';
@@ -23,11 +24,21 @@ function displayTime() {
     if (secs < 10) {
         secs = "0"+ secs;
     }
+    if ((hrs >= 6 && period == 'PM') || (hrs >= 0 && period == 'AM')) {
+        document.body.style.backgroundImage = "url('summer_night.jpeg')";
+        clock.style.color = 'white';
+    }
+    else if ((hrs >= 6 && period == 'AM') || (hrs >= 0 && period == 'PM')) {
+        document.body.style.backgroundImage = "url('summer_day.jpg')";
+        clock.style.color = 'white';
+    }
 
     document.getElementById('hours').innerHTML = hrs;
     document.getElementById('minutes').innerHTML = mins;
     document.getElementById('seconds').innerHTML = secs;
     document.getElementById('sessions').innerHTML = period;
+
+    
 
 }
 setInterval(displayTime, 10);
